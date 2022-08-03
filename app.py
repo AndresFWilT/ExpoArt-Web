b7e6c9b73e2227986c5c0d3a3b85840180966f4d
Andres Felipe Wilches Torres <andresfwilchest@gmail.com>
2022-08-03 00:24:54 -0500

    app.py: added comments

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ from jinja2 import Environment, FileSystemLoader
 import os
 app = Flask(__name__)
 
+# endpoint for the app
 @app.route('/')
 def init():
     return index()
 
+# endpoint for index
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+# endpoint for comunication
 @app.route('/comunication')
 def module_communication():
     return render_template('comunication.html')
 
+# app start
 if __name__ == '__main__':
     app.config.from_object(DevelopmentConfig)
     app.run(debug=True)
